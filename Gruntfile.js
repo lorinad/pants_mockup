daee14f0f236d43244f48b74c93c75ba28aa1b63
lorinad <ldzhamankulova@dons.usfca.edu>
2018-03-26 14:41:18 -0700

    v1

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 700,
+            height: 525,
+            suffix: 'sm',
+            quality: 30
+          }]
+        },
+
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png,jpeg}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
+
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+# Mock up for pantsbuild.org
+
+#### Description
+
+Built a fully responsive site using `HTML`, `CSS` & `Bootstrap` elements
+
